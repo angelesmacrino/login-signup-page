add68fbc520a6c7dbc4f281723e4b3d10d7416a3
Angeles Macrino <63437504+angelesmacrino@users.noreply.github.com>
2021-06-28 16:21:30 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ $(function() {
     let loginStatus = localStorage.getItem('loggedIn')
     if (loginStatus == "true") {
         $(".unlogged a").hide()
-        $(".logged a").show()        
+        $(".logged a").show()
+        $(".logged").show()
+        $(".pageDescription").hide()        
     }
     else {
         $(".unlogged a").show()
